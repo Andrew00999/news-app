3bd306ccf893a9fb5a1ab30f230577c7d8e400e2
Andrew <mysecondmail0099@gmail.com>
2023-08-04 14:41:10 +0300

    change structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Header from "./components/Header";
 
@@ -8,13 +8,12 @@ import NewsDetails from "./pages/NewsDetails";
 function App() {
     return (
         <>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/article/:id" element={<NewsDetails />} />
-                </Routes>
-            </Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/article/:id" element={<NewsDetails />} />
+            </Routes>
             <Header />
+            <Home />
         </>
     );
 }
